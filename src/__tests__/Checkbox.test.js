8412da8ceea0252c885351ba29f3f9a41ab7952d
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-05-10 05:46:08 -0700

    Added Checkbox tests

--- src/__tests__/Checkbox.test.js
+++ src/__tests__/Checkbox.test.js
@@ -10,8 +10,32 @@ describe("checkbox renders", () => {
                 name="avocado-checkbox" 
                 id="avocado-checkbox" 
                 label="avocado checkbox"
+                checked={false}
+                disabled={false}
                 />);
         const div = screen.getByTestId("avocado-checkbox");
         expect(div).toBeDefined();
+    })
+        test("input renders", () => {
+        render(<Checkbox 
+                name="avocado-checkbox" 
+                id="avocado-checkbox" 
+                label="avocado checkbox"
+                checked={false}
+                disabled={false}
+                />);
+        const input = screen.getByLabelText("avocado checkbox");
+        expect(input).toBeDefined();
+    })
+    test("label renders", () => {
+        render(<Checkbox 
+                name="avocado-checkbox" 
+                id="avocado-checkbox" 
+                label="avocado checkbox"
+                checked={false}
+                disabled={false}
+                />);
+        const label = screen.getByText("avocado checkbox");
+        expect(label).toBeDefined();
     })
 })
