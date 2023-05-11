425548fe67c4f9ca86ccba1e84fc853ba734211d
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-05-11 08:57:35 -0700

    Created Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export interface ButtonProps {
+  id: string
+  name: string
+  type: "button" | "reset" | "submit"
+  disabled: boolean
+  buttonText: string
+}
+
+const Button = ({id, name, type, disabled, buttonText}: ButtonProps) => {
+  return (
+      <button
+        id={id}
+        name={name}
+        type={type}
+        disabled={disabled}
+      >{buttonText}</button>
+  )
+};
+
+export default Button;
