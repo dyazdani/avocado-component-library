508b6c9a6ff40906896b30834cbbe21153e23ef9
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-05-11 12:01:52 -0700

    Added TextField component with boilerplate code

---
+++ src/components/TextField/TextField.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const TextField = ({} ) => {
+  
+  return (
+          <input
+            type="text"
+            data-testid="avocado-textfield"
+          />
+        )
+};
+
+export default TextField;
