24e48baf6aa7756078ed0b5c2dc009cbecc38a3d
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-05-13 09:29:54 -0700

    Added Selector component

---
+++ src/components/Selector/Selector.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ThemeContext } from "../Contexts";
+import { useState, useContext } from "react";
+import SelectOption from "../SelectOption/SelectOption";
+
+
+const Selector = ({name, id, valueOne, valueTwo, valueThree}) => {
+  
+  return (
+          <select
+            name={name}
+            id={id}
+            data-testid="avocado-selector">
+            <option
+              value="" 
+              data-testid="avocado-select-option-instructions">--Please choose an option--
+            </option>
+            <SelectOption value={valueOne}/>
+            <SelectOption value={valueTwo}/>
+            <SelectOption value={valueThree}/>
+          </select>
+          
+          
+        )
+};
+
+export default Selector;
