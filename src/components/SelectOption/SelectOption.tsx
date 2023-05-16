57fb9b8ae8040716dbe5ef75f4cc472acb9e3d47
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-05-16 15:40:12 -0700

    Added test for SelectorDiv.tsx

--- src/components/SelectOption/SelectOption.tsx
+++ src/components/SelectOption/SelectOption.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import { ThemeContext } from "../Contexts";
 import { useState, useContext } from "react";
 
+interface SelectOptionProps {
+  value: string
+}
 
-const SelectOption = ({value}) => {
+const SelectOption = ({value}: SelectOptionProps) => {
   
   return (
           <option
             value={value} 
+            name={value}
             data-testid="avocado-select-option-{value}">{value}
           </option>
         )
