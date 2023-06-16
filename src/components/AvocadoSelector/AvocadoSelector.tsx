014c82ecfb23c43c209ddfc403ed3efc0d79f43f
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-06-16 10:15:09 -0700

    Used the children prop with Selector component

--- src/components/AvocadoSelector/AvocadoSelector.tsx
+++ src/components/AvocadoSelector/AvocadoSelector.tsx
@@ -1,31 +1,27 @@
 import React from "react";
 import AvocadoSelect from "../AvocadoSelect/AvocadoSelect";
 import AvocadoLabel from "../AvocadoLabel/AvocadoLabel";
-import { useContext } from "react";
+import { useContext, PropsWithChildren } from "react";
 import { AvocadoThemeContext } from "../AvocadoThemeContext";
 
 export interface AvocadoSelectorProps {
   id: string
   name: string
   label: string
-  valueOne: string
-  valueTwo: string
-  valueThree: string
 }
 
-const AvocadoSelector = ({id, name, label, valueOne, valueTwo, valueThree}: AvocadoSelectorProps) => {
-  const theme = useContext(AvocadoThemeContext)
+const AvocadoSelector = ({
+  id, 
+  name, 
+  label, 
+  children
+  }: PropsWithChildren<AvocadoSelectorProps>) => {
+    const theme = useContext(AvocadoThemeContext)
 
   return (
       <div data-testid="avocado-selector" className={`avocado-selector ${theme}`}>
         <AvocadoLabel htmlFor={id} label={label}/>
-        <AvocadoSelect
-          name={name}
-          id={id}
-          valueOne={valueOne}
-          valueTwo={valueTwo}
-          valueThree={valueThree}
-        />
+        {children}
       </div>
   )
 };
