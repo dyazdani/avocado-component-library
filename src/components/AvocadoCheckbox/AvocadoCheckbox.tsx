b2a551a251958c9b619c8469978fe81da49146a3
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-06-21 09:49:30 -0700

    Moved InputCheckbox to AvocadoCheckbox and controlled component

--- src/components/AvocadoCheckbox/AvocadoCheckbox.tsx
+++ src/components/AvocadoCheckbox/AvocadoCheckbox.tsx
@@ -1,5 +1,4 @@
 import React, {ChangeEvent} from "react";
-import AvocadoInputCheckbox from "../AvocadoInputCheckbox/AvocadoInputCheckbox";
 import AvocadoLabel from "../AvocadoLabel/AvocadoLabel";
 import { AvocadoThemeContext } from "../AvocadoThemeContext";
 import { useContext } from "react";
@@ -8,12 +7,34 @@ export interface AvocadoCheckboxProps {
   label: string
   id: string
   name: string
-  disabled: boolean
-  checked: boolean
+  disabled?: boolean
+  checked?: boolean
+  value: string
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
-const AvocadoCheckbox = ({label, id, name, disabled, checked, onChange}: AvocadoCheckboxProps) => {
+const AvocadoInputCheckbox = ({id, name, disabled, checked, value, onChange}: AvocadoCheckboxProps) => {
+  const theme = useContext(AvocadoThemeContext)
+
+return (
+        <input
+          className={`avocado-input-checkbox ${theme}`}
+          type="checkbox" 
+          name={name} 
+          id={id} 
+          tabIndex={0} 
+          //TODO: Figure out how to make checked attribute appear in html
+          // and work both in testing and in Storybook
+          checked={checked}
+          onChange={onChange}
+          disabled={disabled}
+          value={value}
+          data-testid="avocado-input"
+        />
+      )
+};
+
+const AvocadoCheckbox = ({label, id, name, disabled, checked, value, onChange}: AvocadoCheckboxProps) => {
   const theme = useContext(AvocadoThemeContext)
   return (
       <div className={`avocado-checkbox ${theme}`} data-testid="avocado-checkbox">
@@ -23,6 +44,7 @@ const AvocadoCheckbox = ({label, id, name, disabled, checked, onChange}: Avocado
           checked={checked}
           disabled={disabled}
           onChange={onChange}
+          value={value}
         />
         <AvocadoLabel id={id} label={label}/>
       </div>
