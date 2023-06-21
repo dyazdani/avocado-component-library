731d6088a91fcef90341ed2b36bef0720d082a0d
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-06-21 10:48:06 -0700

    Controlled Button component and added new props

--- src/components/AvocadoButton/AvocadoButton.tsx
+++ src/components/AvocadoButton/AvocadoButton.tsx
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import { useContext } from "react";
 import { AvocadoThemeContext } from "../AvocadoThemeContext";
 
 export interface AvocadoButtonProps {
-  id: string
-  name: string
+  id?: string
+  name?: string
   type: "button" | "reset" | "submit"
-  disabled: boolean
-  buttonText: string
+  disabled?: boolean
+  value?: string
+  buttonText?: string
+  form?: string
+  formAction?: string // TODO: How to I make this type a URL string?
+  formEncType?: "multipart/form-data" | "text/plain"
+  formNoValidate?: boolean
+  formTarget?: "_blank" | "_parent" | "_top" | `_${string}`
+  formMethod?: "post" | "get"
+  popoverTarget?: string
+  popoverTargetAction?: "hide" | "show"
+  autoFocus?: boolean
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
-const AvocadoButton = ({id, name, type, disabled, buttonText}: AvocadoButtonProps) => {
+const AvocadoButton = ({
+  id, 
+  name, 
+  form, 
+  type, 
+  disabled, 
+  autoFocus,
+  formAction,
+  formEncType,
+  formNoValidate,
+  formTarget,
+  formMethod,
+  popoverTarget,
+  popoverTargetAction, 
+  buttonText, 
+  onChange}: AvocadoButtonProps) => {
   const theme = useContext(AvocadoThemeContext)
   return (
       <button
@@ -18,7 +44,17 @@ const AvocadoButton = ({id, name, type, disabled, buttonText}: AvocadoButtonProp
         id={id}
         name={name}
         type={type}
+        form={form}
+        formaction={formAction}
+        formenctype={formEncType}
+        formnovalidate={formNoValidate}
+        formtarget={formTarget}
+        formmethod={formMethod}
+        popovertarget={popoverTarget}
+        popovertargetaction={popoverTargetAction}
         disabled={disabled}
+        autofocus={autoFocus}
+        onChange={onChange}
         data-testid="avocado-button"
       >{buttonText}</button>
   )
