9a230d8f216bb7de14c909301634252a4f75f593
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-06-22 17:12:15 -0700

    Removing unnecessary button props

--- src/components/AvocadoButton/AvocadoButton.tsx
+++ src/components/AvocadoButton/AvocadoButton.tsx
@@ -9,32 +9,14 @@ export interface AvocadoButtonProps {
   disabled?: boolean
   value?: string
   buttonText?: string
-  form?: string
-  formAction?: string
-  formEncType?: "multipart/form-data" | "text/plain"
-  formNoValidate?: boolean
-  formTarget?: "_blank" | "_parent" | "_top" | `_${string}`
-  formMethod?: "post" | "get"
-  popoverTarget?: string
-  popoverTargetAction?: "hide" | "show"
-  autoFocus?: boolean
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
 const AvocadoButton = ({
   id, 
   name, 
-  form, 
   type, 
   disabled, 
-  autoFocus,
-  formAction,
-  formEncType,
-  formNoValidate,
-  formTarget,
-  formMethod,
-  popoverTarget,
-  popoverTargetAction, 
   buttonText, 
   onChange}: AvocadoButtonProps) => {
   const theme = useContext(AvocadoThemeContext)
@@ -44,16 +26,7 @@ const AvocadoButton = ({
         id={id}
         name={name}
         type={type}
-        form={form}
-        formAction={formAction}
-        formEncType={formEncType}
-        formNoValidate={formNoValidate}
-        formTarget={formTarget}
-        formMethod={formMethod}
-        popoverTarget={popoverTarget}
-        popoverTargetAction={popoverTargetAction}
         disabled={disabled}
-        autofocus={autoFocus}
         onChange={onChange}
         data-testid="avocado-button"
       >{buttonText}</button>
