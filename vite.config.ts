ce832280f709ad0d313ed5c79c07f5eba70a0a6e
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-06-27 11:08:00 -0700

    Configured Vite to use MDX files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import mdx from '@mdx-js/rollup'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    mdx(/* jsxImportSource: …, otherOptions… */)
+  ],
 })
