9f57a88ae4948b2dab587e846a0110bc7da9ce72
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-06-28 15:34:36 -0700

    Added story to AvocadoRadioGroupItem

--- src/components/AvocadoRadioGroupItem/AvocadoRadioGroupItem.stories.tsx
+++ src/components/AvocadoRadioGroupItem/AvocadoRadioGroupItem.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import AvocadoRadioGroupItem from './AvocadoRadioGroupItem';
+import React from 'react';
 
 const meta: Meta<typeof AvocadoRadioGroupItem> = {
     title: 'AvocadoRadioGroupItem',
@@ -10,10 +11,19 @@ export default meta;
 type Story = StoryObj<typeof AvocadoRadioGroupItem>;
 
 
+
 export const Primary: Story = {
+    render: () => <AvocadoRadioGroupItem label="Avocado Radio Button" name="avocado-radio-group-item"/>
+}
+
+//TODO: can I get disabled story to work? The problem is this render does not make the input radio element have the disabled attribute, it affects the AvocadoRadioGroupItem component which is a <div></div>
+
+// export const Disabled: Story = {
+//     render: () => <AvocadoRadioGroupItem disabled={true} label="Avocado Radio Button" name="avocado-radio-group-item"/>
+// }
+
+export const Playground: Story = {
     args: {
-        label: "Add label text",
-        id: "avocado-radio-group-item",
-        name: "avocado-radio-group-item",
+        label: "Edit label text"
     }
 }
