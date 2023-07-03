a459bc11bc84038c49805a4b85920c3f2f051811
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-07-03 14:25:19 -0700

    Adding rollup plugin to jandle CSS

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,6 +4,7 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
 import packageJson from "./package.json" assert { type: "json" };
+import postcss from "rollup-plugin-postcss";
 
 export default [
   {
@@ -25,11 +26,13 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
